1514102c73d9020d2656e1df9b1c99a81ec84448
Priyanshu Bhandari <technopriyu@gmail.com>
2022-07-10 23:29:39 +0530

    Cleaned folder and added favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    // const fetchData = async () => {
+    //   const data = await fetch(
+    //     'https://api.genius.com/songs/5830307?access_token=process.env.REACT_APP_CLIENT_ACCESS_TOKEN'
+    //   );
+    //   const res = await data.json();
+    //   console.log(res);
+    // };
+    // fetchData();
+  }, []);
+
+  return <div>HHhhh</div>;
 }
 
 export default App;
