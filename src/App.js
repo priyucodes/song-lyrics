e20bf46fe323865de30f91c9c0bb8369a293ae0b
Priyanshu Bhandari <technopriyu@gmail.com>
2022-07-11 19:29:44 +0530

    added modal

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ import styled from 'styled-components';
 import Navbar from './components/Navbar';
 import SongDetails from './components/SongDetails';
 import SongsList from './components/SongsList';
-
+import Modal from 'react-modal';
+const customStyles = {
+  overlay: {
+    background: 'rgba(0, 0, 0, 0.75)',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#1d1d1d',
+  },
+};
+Modal.setAppElement('#modal');
 function App() {
   const [searchedData, setSearchedData] = useState([]);
+
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   if (searchedData?.response?.hits.length === 0) {
     toast.error('No songs found', {
@@ -22,6 +39,9 @@ function App() {
     });
   }
 
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <ToastContainer
@@ -31,6 +51,29 @@ function App() {
         pauseOnHover
         style={{ fontSize: '2rem' }}
       />
+      <StyledModalButton onClick={() => setIsOpen(true)}>
+        Note
+      </StyledModalButton>
+      {modalIsOpen && (
+        <Modal
+          isOpen={modalIsOpen}
+          style={customStyles}
+          onRequestClose={closeModal}
+          // portalClassName="modal"
+        >
+          <StyledDMCA>
+            <h2>DMCA</h2>
+            <p>
+              This website is not responsible for content displayed within
+              pages, videos, image, music etc. All videos,music etc. is taken
+              from 3rd party sites.
+              <br />
+              If you have any legal issues please contact appropriate media file
+              owners/ hosters
+            </p>
+          </StyledDMCA>
+        </Modal>
+      )}
       <Routes>
         <Route
           path="/search"
@@ -78,4 +121,26 @@ const StyledParagraph = styled.p`
   text-align: center;
   margin-top: 10rem;
   color: #1d1d1d;
+`;
+const StyledModalButton = styled.button`
+  padding: 2rem;
+  background-color: transparent;
+  border: 1px solid #1d1d1d;
+  color: #fff;
+  transition: all 0.5s;
+  &:hover {
+    background-color: #1d1d1d;
+    color: #fff;
+  }
+`;
+
+const StyledDMCA = styled.div`
+  height: 300px;
+  text-align: center;
+  color: goldenrod;
+  p {
+    margin-top: 2rem;
+    font-size: 2rem;
+    color: #fff;
+  }
 `;
--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -8,6 +8,7 @@ const SongDetails = () => {
   const [song, setSong] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [reactPlayer, setReactPlayer] = useState(null);
+  const [showVideo, setShowVideo] = useState(false);
   const [lyrics, setLyrics] = useState('');
   const { id, song: songPath } = useParams();
   useEffect(() => {
@@ -26,10 +27,14 @@ const SongDetails = () => {
   };
   const playSongsHandler = async song => {
     setIsPlaying(!isPlaying);
+    const displayStyle = {
+      display: showVideo ? 'block' : 'none',
+    };
     setReactPlayer(
       <ReactPlayer
         url={song.response.song.media[0].url}
-        playing={isPlaying ? true : false}
+        playing={!isPlaying ? true : false}
+        style={displayStyle}
         config={{
           youtube: {
             playerVars: { showinfo: 1 },
@@ -40,6 +45,10 @@ const SongDetails = () => {
   };
   const pauseHandler = () => {
     setReactPlayer(null);
+    setIsPlaying(false);
+  };
+  const showVideoHandler = () => {
+    setShowVideo(prevState => !prevState);
   };
   return (
     <Container>
@@ -94,7 +103,6 @@ const SongDetails = () => {
                   >
                     <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm115.7 272l-176 101c-15.8 8.8-35.7-2.5-35.7-21V152c0-18.4 19.8-29.8 35.7-21l176 107c16.4 9.2 16.4 32.9 0 42z"></path>
                   </svg>
-                  <audio src={song.response.song.media[0].url}></audio>
                 </>
               )}
             </PlayButton>
@@ -111,6 +119,19 @@ const SongDetails = () => {
                 <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm96 328c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V176c0-8.8 7.2-16 16-16h160c8.8 0 16 7.2 16 16v160z"></path>
               </svg>
             </PauseButton>
+            <ShowVideoButton onClick={showVideoHandler}>
+              <svg
+                id="Layer_1"
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 122.88 111.34"
+                fill="currentColor"
+                stroke="currentColor"
+              >
+                <title>video</title>
+                <path d="M23.59,0h75.7a23.68,23.68,0,0,1,23.59,23.59V87.75A23.56,23.56,0,0,1,116,104.41l-.22.2a23.53,23.53,0,0,1-16.44,6.73H23.59a23.53,23.53,0,0,1-16.66-6.93l-.2-.22A23.46,23.46,0,0,1,0,87.75V23.59A23.66,23.66,0,0,1,23.59,0ZM54,47.73,79.25,65.36a3.79,3.79,0,0,1,.14,6.3L54.22,89.05a3.75,3.75,0,0,1-2.4.87A3.79,3.79,0,0,1,48,86.13V50.82h0A3.77,3.77,0,0,1,54,47.73ZM7.35,26.47h14L30.41,7.35H23.59A16.29,16.29,0,0,0,7.35,23.59v2.88ZM37.05,7.35,28,26.47H53.36L62.43,7.38v0Zm32,0L59.92,26.47h24.7L93.7,7.35Zm31.32,0L91.26,26.47h24.27V23.59a16.32,16.32,0,0,0-15.2-16.21Zm15.2,26.68H7.35V87.75A16.21,16.21,0,0,0,12,99.05l.17.16A16.19,16.19,0,0,0,23.59,104h75.7a16.21,16.21,0,0,0,11.3-4.6l.16-.18a16.17,16.17,0,0,0,4.78-11.46V34.06Z" />
+              </svg>
+            </ShowVideoButton>
             {reactPlayer && reactPlayer}
           </Controls>
           {lyrics.length > 1 && <LyricsPara>{lyrics}</LyricsPara>}
@@ -158,6 +179,9 @@ const Controls = styled.div`
 `;
 const PlayButton = styled.button``;
 const PauseButton = styled.button`
+  margin-left: 20px;
+`;
+const ShowVideoButton = styled.button`
   margin-left: 20px;
 `;
 
